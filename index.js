70067d3ce91b49440a0cd692be878af2ce834e4d
Rácz Viktor <raczviktor0220@gmail.com>
2023-03-05 19:28:46 +0100

    colors has been setted

--- index.js
+++ index.js
@@ -8,73 +8,72 @@ const notFound = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    
+
     const city = document.querySelector('.search-box input').value;
 
     if (city === '') return;
 
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
-        ).then(res=>res.json()).then(json=> {
-       if (json.cod === '404') {
-        container.style.height = '400px';
-        weatherBox.style.display = 'none';
-        weatherDetails.style.display = 'none';
-        notFound.style.display = 'block';
-        notFound.classList.add('fadeIn');
-        return;
-       }
+    ).then(res => res.json()).then(json => {
+        if (json.cod === '404') {
+            container.style.height = '420px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            notFound.style.display = 'block';
+            notFound.classList.add('fadeIn');
+            return;
+        }
 
-       notFound.style.display = 'none';
-       notFound.classList.remove('fadeIn'); 
+        notFound.style.display = 'none';
+        notFound.classList.remove('fadeIn');
 
-       const image = document.querySelector(
-        '.weather-box img'
+        const image = document.querySelector(
+            '.weather-box img'
         );
-       const temperature = document.querySelector(
-        '.weather-box .temperature'
+        const temperature = document.querySelector(
+            '.weather-box .temperature'
         );
-       const description = document.querySelector(
-        '.weather-box .description'
+        const description = document.querySelector(
+            '.weather-box .description'
         );
-       const humidity = document.querySelector(
-        '.weather-details .humidity span'
+        const humidity = document.querySelector(
+            '.weather-details .humidity span'
         );
-       const wind = document.querySelector(
-        '.weather-details .wind span'
+        const wind = document.querySelector(
+            '.weather-details .wind span'
         );
-    
-        console.log(json.weather[0].main)
-       switch (json.weather[0].main){
-        case 'Clear':
-            image.src = 'images/sunny.png'; 
-            break;
-        case 'Rain':    
-            image.src = 'images/rain.png'; 
-            break;
-        case 'Snow':    
-            image.src = 'images/snow.png'; 
-            break;
-        case 'Clouds':    
-            image.src = 'images/clouds.png'; 
-            break;
-        case 'Extreme':    
-            image.src = 'images/storm.png'; 
-            break;
-        default:
-            image.src = '';
-            break;    
-       }
 
-       temperature.innerHTML = `${parseInt(json.main.temp)} <span>C</span>`;
-       description.innerHTML = `${json.weather[0].description}`;
-       humidity.innerHTML = `${json.main.humidity}%`;
-       wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = 'images/sunny.png';
+                break;
+            case 'Rain':
+                image.src = 'images/rain.png';
+                break;
+            case 'Snow':
+                image.src = 'images/snow.png';
+                break;
+            case 'Clouds':
+                image.src = 'images/clouds.png';
+                break;
+            case 'Extreme':
+                image.src = 'images/storm.png';
+                break;
+            default:
+                image.src = '';
+                break;
+        }
 
-       weatherBox.style.display = '';
-       weatherDetails.style.display = '';
-       weatherBox.classList.add('fadeIn');
-       weatherDetails.classList.add('fadeIn');
-       container.style.height = '590px';
+        temperature.innerHTML = `${parseInt(json.main.temp)} <span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display = '';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '590px';
     })
 })
